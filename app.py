ff172afe4e5c4b817f4c7d555a1533172749a5fd
Mohit Ahuja <78975194+mohitahujaa@users.noreply.github.com>
2025-08-28 15:46:54 +0530

    Configure CORS in Flask app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.skills import skills_bp   # import blueprint
+from routes.test import test_bp
+from routes.skills import skills_bp
 
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(skills_bp, url_prefix="/skills")
 
-if __name__ == "__main__":
-    app.run(debug=True) 
+# Register blueprints
+app.register_blueprint(test_bp)
+app.register_blueprint(skills_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
