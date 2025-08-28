cac9c5fb755f134be357cab506b768bbdb069cd7
Mohit Ahuja <78975194+mohitahujaa@users.noreply.github.com>
2025-08-28 15:46:25 +0530

    Fix CORS error in Flask app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.skills import skills_bp   # import blueprint
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(skills_bp, url_prefix="/skills")
 
 if __name__ == "__main__":
